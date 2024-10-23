819824c70e1fec0943804944003c56f4f4e885f9
Jhoan Oswaldo Ome Vega <Jhoan-Oswaldo.Ome@endava.com>
2024-10-23 14:15:09 -0500

    Add singleton pattern

---
+++ Design patterns/Singlenton/Database.cs
@@ -0,0 +1,26 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Singleton
+{
+    sealed class Database
+    {
+        private static Database _instance;
+        private static readonly object _lock = new object();
+        private string _value = "Default value";
+        private Database() { }
+            
+        [MethodImpl(MethodImplOptions.Synchronized)]
+        public static Database GetInstance() {
+            if (_instance == null) {
+                lock (_lock) {
+                    _instance = new Database();
+                }
+            }
+            return _instance;
+        }
+
+        public string Value { get => _value; set => _value = value; }
+
+
+    }
+}
---
+++ Design patterns/Singlenton/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Singleton;
+
+Thread t1 = new Thread(() => {
+    Database foo = Database.GetInstance();
+    foo.Value = "Object 1";
+    Console.WriteLine(foo.Value);
+});
+Thread t2 = new Thread(() => {
+    Database bar = Database.GetInstance();
+    Console.WriteLine(bar.Value);
+    bar.Value = "Change all";
+    Console.WriteLine(bar.Value);
+});
+
+t1.Start();
+t2.Start();
+
+t1.Join();
+t2.Join();
+
+
+    
+
+    
+
+
+
+
+//Database finalObject = new Database(); it fails!
